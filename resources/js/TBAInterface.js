04a723b1786d9bc0c49081ff31078663e20a0466
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2024-01-27 13:18:02 -0500

    updated TBAAuthKey

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "FsOtKBdeianq3XRCZDPy1p5g9zpycVcU8KstpVCgkqD3OsnT44GtdIw1Qax4TpFi";
 /**
  * Get list of teams in event
  *
